11ed9df7d06a3037eae150a36db7ccf381fb21dc
Hoiry <hoil20707@gmail.com>
2023-08-06 11:47:44 +0900

    no message

--- js/mrz-helper.js
+++ js/mrz-helper.js
@@ -1,14 +1,17 @@
-let row1Pattern = /P[A-Z0-9<][A-Z]{3}([A-Z0-9<]{39})/g;
-let row2Pattern = /([A-Z0-9][A-Z0-9<]{8})[0-9]([A-Z]{3})([0-9]{6})[0-9][MF<]([0-9]{6})[0-9][A-Z0-9<]{14}[0-9<][0-9]/g;
+let row1Pattern = /P[A-Z0-9<][A-Z<]{3}([A-Z0-9<]{39})/g;
+let row2Pattern = /([A-Z0-9][A-Z0-9<]{8})[0-9]([A-Z<]{3})([0-9]{6})[0-9][MF<]([0-9]{6})[0-9][A-Z0-9<]{14}[0-9<][0-9]/g;
 
 function getMRZCode(ocrText) {
     let result = [];
     let ocrTextList = ocrText.split("\n");
+    ocrTextList = ocrTextList.map(item => item.replace(/\s/g, '')).filter(item => item.length >= 44);
+    console.log(ocrTextList);
     let index = ocrTextList.length - 1;
+    
 
     // 2번째 줄의 MRZ 찾기
     for (let i = index; i >= 0; i--) {
-        let row2 = ocrTextList[i].replace(/\s/g, '').match(row2Pattern);
+        let row2 = ocrTextList[i].match(row2Pattern);
 
         if (row2 != null) {
             result[1] = row2[0];
@@ -20,7 +23,7 @@ function getMRZCode(ocrText) {
     // 1번째 줄의 MRZ 찾기
     if (result[1] != undefined) {
         for(let j = index - 1;  j >= 0; j--) {
-            let row1 = ocrTextList[j].replace(/\s/g, '').match(row1Pattern);
+            let row1 = ocrTextList[j].match(row1Pattern);
             
             if(row1 != null) {
                 result[0] = row1[0];
